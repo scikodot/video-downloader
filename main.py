cec8385512ac1457e012765c0afae0db542b7c76
Dan <daniladenisov729@gmail.com>
2025-02-14 21:33:30 +0300

    Abstract from audio/video types, add filename to output path if absent

--- main.py
+++ main.py
@@ -33,15 +33,23 @@ def validate_output_path(output_path):
     
     return output_path
 
+def get_av_urls(drv):
+        network_logs = drv.execute_script("return window.performance.getEntriesByType('resource');")
+        links = []
+        for network_log in network_logs:
+            initiator_type = network_log.get('initiatorType', '')
+            name = network_log.get('name', '')
+            if initiator_type == 'fetch' and name != '' and (bytes_pos := name.find('bytes=0-')) > 0:
+                links.append((name, bytes_pos))
+            
+        return links[:2] if len(links) >= 2 else False
+
 def main():
     parser = argparse.ArgumentParser(prog='video-downloader')
     parser.add_argument('url', help="Video URL", type=validate_url)
-    parser.add_argument('-o', '--output-path', help="Output path", type=validate_output_path)
+    parser.add_argument('-o', '--output-path', help="Output path", default=get_default_output_path(), type=validate_output_path)
     parser.add_argument('-v', '--verbose', help="Show debug info", action='store_true')
     args = parser.parse_args()
-
-    if args.output_path is None:
-        args.output_path = get_default_output_path()
 
     if args.verbose:
         print("Args:", vars(args), end='\n\n')
@@ -60,17 +68,6 @@ def main():
     # options.add_argument('--disable-web-security')  # Disable web security
     # options.add_argument('--allow-running-insecure-content')  # Allow running insecure content
     # options.add_argument('--disable-webrtc')  # Disable WebRTC
-
-    def get_av_urls(drv):
-        network_logs = drv.execute_script("return window.performance.getEntriesByType('resource');")
-        links = []
-        for network_log in network_logs:
-            initiator_type = network_log.get('initiatorType', '')
-            name = network_log.get('name', '')
-            if initiator_type == 'fetch' and name != '' and (bytes_pos := name.find('bytes=0-')) > 0:
-                links.append((name, bytes_pos))
-            
-        return links[:2] if len(links) >= 2 else False
 
     # Request the URL and wait for 20 secs or until we get both audio and video URLs
     driver = webdriver.Chrome(options=options)
@@ -95,6 +92,7 @@ def main():
         session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
 
     # Download files from the obtained URLs, with byte range step of 1 MB, into a temporary directory
+    filepaths = {}
     with tempfile.TemporaryDirectory() as temp_dir:
         if args.verbose:
             print("Temporary directory:", temp_dir)
@@ -116,21 +114,31 @@ def main():
                 print("Headers:", headers, end='\n\n')
 
             content_type = headers.get('Content-Type', '')
-            if content_type != 'video/mp4' and content_type != 'audio/mp4':
+            filepath = pathlib.Path(temp_dir) / content_type.replace('/', '.')
+            if args.verbose:
+                print("Filepath:", str(filepath), end='\n\n')
+            
+            if content_type.startswith('audio'):
+                filepaths['audio'] = filepath
+            elif content_type.startswith('video'):
+                filepaths['video'] = filepath
+            else:
                 raise ValueError("Inappropriate MIME-type.")
             
-            with open(os.path.join(temp_dir, content_type.replace('/', '.')), 'wb+') as f:
+            with open(filepath, 'wb+') as f:
                 for chunk in response.iter_content(chunk_size=128):
                     f.write(chunk)
 
+        # Attach the video file name if the output path is a directory
+        if pathlib.Path(args.output_path).suffix == '':
+            args.output_path = os.path.join(args.output_path, filepaths['video'].name)
+
         # Merge the downloaded files into one (audio + video)
-        audio_path = os.path.join(temp_dir, 'audio.mp4')
-        video_path = os.path.join(temp_dir, 'video.mp4')
         with (
-            AudioFileClip(audio_path) as audio, 
-            VideoFileClip(video_path) as video
+            AudioFileClip(filepaths['audio']) as audio, 
+            VideoFileClip(filepaths['video']) as video
         ):
-            video.with_audio(audio).write_videofile(args.output_path)
+            video.with_audio(audio).write_videofile(args.output_path, codec='libx264')
     
     return
 
